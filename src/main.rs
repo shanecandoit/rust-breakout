87081377c48b081304d9fc5d8ca8c12190190226
outOfBounds <shanecandoit@gmail.com>
2023-03-29 10:45:08 -0500

    better collision handling, change colors, rename player to paddle

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn window_config() -> Conf {
 #[macroquad::main(window_config)]
 async fn main() {
     let score_text = "Score: 0";
-    let mut player = Player::new_default();
+    let mut player = Paddle::new_default();
     let mut ball = Ball::new_default();
 
     loop {
@@ -23,17 +23,24 @@ async fn main() {
         player.update();
         ball.update();
 
+        // check for collision
+        let (is_collide, _cx, cy) = collisions(&mut ball, &mut player);
+        if is_collide {
+            // push ball outside paddle
+            ball.y += cy;
+            // reverse ball direction
+            ball.dy = -ball.dy;
+        }
+
+        // space bar spawns a new ball to make debugging easier
+        if is_key_pressed(KeyCode::Space) {
+            ball = Ball::new_default();
+        }
+
         // draw
         player.draw();
         ball.draw();
         draw_ui(score_text);
-
-        // do the ball and player collide?
-        if ball.x + ball.w > player.x && ball.x < player.x + player.width {
-            if ball.y + ball.h > player.y && ball.y < player.y + player.height {
-                ball.dy = -ball.dy;
-            }
-        }
 
         // quit
         if is_key_down(KeyCode::Escape) {
@@ -42,14 +49,14 @@ async fn main() {
 
         next_frame().await;
 
-        // wait?
+        // wait? native is faster than wasm, wasm can't sleep
         if is_native() && get_fps() > 60 {
             thread::sleep(time::Duration::from_millis(1));
         }
     }
 }
 
-struct Player {
+struct Paddle {
     x: f32,
     y: f32,
     width: f32,
@@ -57,15 +64,15 @@ struct Player {
     color: Color,
 }
 
-impl Player {
-    fn new_default() -> Player {
+impl Paddle {
+    fn new_default() -> Paddle {
         let w = 200_f32;
-        Player {
+        Paddle {
             x: screen_width() / 2.0 - w / 2.0,
             y: 400f32,
             width: w,
             height: 50f32,
-            color: RED,
+            color: BLUE,
         }
     }
 
@@ -116,7 +123,7 @@ impl Ball {
             dy: ry,
             w: 32_f32,
             h: 32_f32,
-            color: WHITE,
+            color: RED,
         }
     }
 
@@ -145,7 +152,7 @@ impl Ball {
 
 // is the os native or wasm?
 fn is_native() -> bool {
-    let os = env::consts::OS;
+    let os = env::consts::OS.to_lowercase();
     if os == "windows" || os == "macos" || os == "linux" {
         return true;
     }
@@ -153,11 +160,30 @@ fn is_native() -> bool {
 }
 
 fn draw_ui(score_text: &str) {
-    let _sh = screen_height();
     let sw = screen_width();
 
+    // draw the score, centered
     let text_dim = measure_text(score_text, None, 32, 1.0);
     let text_w = text_dim.width;
     let text_x = sw / 2.0 - text_w / 2.0;
     draw_text(score_text, text_x, 30.0, 32.0, WHITE);
+}
+
+fn collisions(ball: &mut Ball, paddle: &mut Paddle) -> (bool, f32, f32) {
+    // https://math.stackexchange.com/questions/99565/simplest-way-to-calculate-the-intersect-area-of-two-rectangles
+
+    // x_overlap and y_overlap are the width and height of the overlap rectangle. postive
+    let mut x_overlap = (ball.x + ball.w).min(paddle.x + paddle.width) - (ball.x).max(paddle.x);
+    let mut y_overlap = (ball.y + ball.h).min(paddle.y + paddle.height) - (ball.y).max(paddle.y);
+    if x_overlap > 0f32 && y_overlap > 0f32 {
+        if ball.x < paddle.x {
+            x_overlap = -x_overlap
+        }
+        if ball.y < paddle.y {
+            y_overlap = -y_overlap
+        }
+        return (true, x_overlap, y_overlap);
+    }
+
+    (false, 0f32, 0f32)
 }
