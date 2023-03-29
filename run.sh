fcf6fe9127c8ea031cf5ac61172a10f9b9b58f74
outOfBounds <shanecandoit@gmail.com>
2023-03-29 13:27:46 -0500

    run script

--- run.sh
+++ run.sh
@@ -7,24 +7,24 @@ cargo fmt
 cargo build
 
 # windows
-rustup  target add      x86_64-pc-windows-gnu
-cargo   build --target  x86_64-pc-windows-gnu
-
-# linux
-# rustup  target add      x86_64-unknown-linux-gnu
-# cargo   build --target  x86_64-unknown-linux-gnu
-
-# wasm
-rustup  target add      wasm32-unknown-unknown
-cargo   build --target  wasm32-unknown-unknown
+# rustup  target add      x86_64-pc-windows-gnu
+# cargo   build           --target  x86_64-pc-windows-gnu
+cargo   build --release --target  x86_64-pc-windows-gnu &
+cargo   run   --release --target  x86_64-pc-windows-gnu &
 
 echo
 echo "Size:"
-#./target/debug/breakout.exe
 ls -lh ./target/debug/breakout.exe
-ls -lh ./target/wasm32-unknown-unknown/debug/breakout.wasm
 
-echo
-echo "Run:"
-cargo install basic-http-server
-basic-http-server .
+# wasm
+# echo "build_wasm 21"
+# # rustup  target add      wasm32-unknown-unknown &
+cargo   build --target  wasm32-unknown-unknown &
+# echo "Size Wasm:"
+ls -lh ./target/wasm32-unknown-unknown/debug/breakout.wasm
+# echo "Run:"
+# cargo install basic-http-server
+# basic-http-server .
+
+date
+echo done
